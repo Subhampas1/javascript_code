0ce8e695c52a829381369b9d884526457df9deb3
Subhampas1 <123561895+Subhampas1@users.noreply.github.com>
2024-05-01 04:30:00 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,25 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+
+let isLoggedIn = "subh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true 
+// "" => false
+// "subh" => true
+
+
+let someNumber  = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
