467655579606f7475dcadc0966c1a4a1905ad1b7
Subhampas1 <123561895+Subhampas1@users.noreply.github.com>
2024-05-01 04:12:28 +0000

    datatupes

---
+++ basics/datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // used in browser
+
+
+let name = "subham"
+let age = 20
+let isLoggedIn = false
+
+console.log(typeof "subham");
---
+++ basics/variables.js
@@ -0,0 +1,4 @@
+const accountId = 437632
+let accountEmail = "subham@gmail.com"
+var accountPassword = "12324"
+accountCity = "dhanbad"
